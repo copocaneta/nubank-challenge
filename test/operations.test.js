52332ae993dcd25d7ee2adcaf3b0f6cdd450d293
Thiago Bernardi <copocaneta@gmail.com>
2024-07-21 18:43:05 -0300

    test: fixing the unit tests returns and adding aditional unit tests

--- test/operations.test.js
+++ test/operations.test.js
@@ -14,7 +14,7 @@ describe("Operations Class", () => {
             quantity: 100,
         });
         const taxes = operationService.getTaxes();
-        expect(taxes).toEqual([{ tax: "0.00" }]);
+        expect(taxes).toEqual([{ tax: 0 }]);
     });
 
     test("should add a sell operation with value less than 20000 and calculate zero tax", () => {
@@ -29,7 +29,7 @@ describe("Operations Class", () => {
             quantity: 50,
         });
         const taxes = operationService.getTaxes();
-        expect(taxes).toEqual([{ tax: "0.00" }, { tax: "0.00" }]);
+        expect(taxes).toEqual([{ tax: 0 }, { tax: 0 }]);
     });
 
     test("should add a sell operation with profit and calculate tax correctly", () => {
@@ -44,7 +44,7 @@ describe("Operations Class", () => {
             quantity: 5000,
         });
         const taxes = operationService.getTaxes();
-        expect(taxes).toEqual([{ tax: "0.00" }, { tax: "10000.00" }]);
+        expect(taxes).toEqual([{ tax: 0 }, { tax: 10000 }]);
     });
 
     test("should handle losses correctly", () => {
@@ -64,10 +64,51 @@ describe("Operations Class", () => {
             quantity: 5000,
         });
         const taxes = operationService.getTaxes();
-        expect(taxes).toEqual([
-            { tax: "0.00" },
-            { tax: "10000.00" },
-            { tax: "0.00" },
-        ]);
+        expect(taxes).toEqual([{ tax: 0 }, { tax: 10000 }, { tax: 0 }]);
+    });
+
+    test("should handle an empty operations", () => {
+        const taxes = operationService.getTaxes();
+        expect(taxes).toEqual([]);
+    });
+
+    test("should handle multiple buy operations and no sell", () => {
+        operationService.addOperation({
+            operation: "buy",
+            unitCost: 10.0,
+            quantity: 100,
+        });
+        operationService.addOperation({
+            operation: "buy",
+            unitCost: 20.0,
+            quantity: 200,
+        });
+        const taxes = operationService.getTaxes();
+        expect(taxes).toEqual([{ tax: 0 }, { tax: 0 }]);
+    });
+
+    test("should handle a sell operation eith no prior buy", () => {
+        operationService.addOperation({
+            operation: "sell",
+            unitCost: 15.0,
+            quantity: 50,
+        });
+        const taxes = operationService.getTaxes();
+        expect(taxes).toEqual([{ tax: 0 }]);
+    });
+
+    test("should handle zero quantity operations", () => {
+        operationService.addOperation({
+            operation: "buy",
+            unitCost: 10.0,
+            quantity: 0,
+        });
+        operationService.addOperation({
+            operation: "sell",
+            unitCost: 15.0,
+            quantity: 0,
+        });
+        const taxes = operationService.getTaxes();
+        expect(taxes).toEqual([{ tax: 0 }, { tax: 0 }]);
     });
 });

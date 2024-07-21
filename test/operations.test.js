23daf6879130b284c5bc3b6a60246ba659714dda
Thiago Bernardi <copocaneta@gmail.com>
2024-07-21 14:22:07 -0300

    test: adding unit tests

---
+++ test/operations.test.js
@@ -0,0 +1,73 @@
+import { Operations } from "../operations";
+
+describe("Operations Class", () => {
+    let operationService;
+
+    beforeEach(() => {
+        operationService = new Operations();
+    });
+
+    test("should add a buy operation and calculate zero tax", () => {
+        operationService.addOperation({
+            operation: "buy",
+            unitCost: 10.0,
+            quantity: 100,
+        });
+        const taxes = operationService.getTaxes();
+        expect(taxes).toEqual([{ tax: "0.00" }]);
+    });
+
+    test("should add a sell operation with value less than 20000 and calculate zero tax", () => {
+        operationService.addOperation({
+            operation: "buy",
+            unitCost: 10.0,
+            quantity: 100,
+        });
+        operationService.addOperation({
+            operation: "sell",
+            unitCost: 15.0,
+            quantity: 50,
+        });
+        const taxes = operationService.getTaxes();
+        expect(taxes).toEqual([{ tax: "0.00" }, { tax: "0.00" }]);
+    });
+
+    test("should add a sell operation with profit and calculate tax correctly", () => {
+        operationService.addOperation({
+            operation: "buy",
+            unitCost: 10.0,
+            quantity: 10000,
+        });
+        operationService.addOperation({
+            operation: "sell",
+            unitCost: 20.0,
+            quantity: 5000,
+        });
+        const taxes = operationService.getTaxes();
+        expect(taxes).toEqual([{ tax: "0.00" }, { tax: "10000.00" }]);
+    });
+
+    test("should handle losses correctly", () => {
+        operationService.addOperation({
+            operation: "buy",
+            unitCost: 10.0,
+            quantity: 10000,
+        });
+        operationService.addOperation({
+            operation: "sell",
+            unitCost: 20.0,
+            quantity: 5000,
+        });
+        operationService.addOperation({
+            operation: "sell",
+            unitCost: 5.0,
+            quantity: 5000,
+        });
+        const taxes = operationService.getTaxes();
+        expect(taxes).toEqual([
+            { tax: "0.00" },
+            { tax: "10000.00" },
+            { tax: "0.00" },
+        ]);
+    });
+});
